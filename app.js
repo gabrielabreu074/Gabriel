92f36759f8acb2bf8e4d7f0fa4af2365b7885b4a
gabrielabreu074 <gabrielabreu074@gmail.com>
2025-11-17 16:30:53 -0300

    Gabriel

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.use(cors());
 // Middleware para ler JSON do corpo da requisição
 app.use(express.json());
 
+const _dirname = path.resolver();
 
-
+app.use(express.static(path.join(_dirname, "public")))
 
 // GET - Consulta os dados cadastrados
 app.get ("/agendamentos", async (req, res)=>{
